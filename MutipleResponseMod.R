ac33193a365d2a5f779c874bffbe276d7e5701d4
Vincent Jalby <vincent.jalby@orange.fr>
2023-11-17 23:40:14 +0100

    Version 0.9.6

--- MutipleResponseMod.R
+++ MutipleResponseMod.R
@@ -1,3 +1,6 @@
+install.packages('jmvtools', repos=c('https://repo.jamovi.org', 'https://cran.r-project.org'))
+jmvtools::check()
+
 jmvtools::create('MultipleResponse')
 
 jmvtools::addAnalysis(name='multipleresponse', title='Multiple Response')
